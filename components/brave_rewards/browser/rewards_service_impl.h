b8e5bc57c420f2087047ff7faa6583eea91d017b
NejcZdovc <nejc.zdovc@3zsistemi.si>
2019-12-18 12:26:39 +0100

    Adds client code for db

--- components/brave_rewards/browser/rewards_service_impl.h
+++ components/brave_rewards/browser/rewards_service_impl.h
@@ -69,6 +69,7 @@ class BraveRewardsBrowserTest;
 namespace brave_rewards {
 
 class PublisherInfoDatabase;
+class RewardsDatabase;
 class RewardsNotificationServiceImpl;
 class BraveRewardsBrowserTest;
 
@@ -804,6 +805,10 @@ class RewardsServiceImpl : public RewardsService,
       const ledger::Result result,
       ledger::BalanceReportInfoPtr report);
 
+  void OnRunDBTransaction(
+      ledger::RunDBTransactionCallback callback,
+      ledger::DBCommandResponsePtr response);
+
 #if defined(OS_ANDROID)
   ledger::Environment GetServerEnvironmentForAndroid();
   void CreateWalletAttestationResult(
@@ -829,9 +834,11 @@ class RewardsServiceImpl : public RewardsService,
   const base::FilePath ledger_state_path_;
   const base::FilePath publisher_state_path_;
   const base::FilePath publisher_info_db_path_;
+  const base::FilePath publisher_info_db_path2_;
   const base::FilePath publisher_list_path_;
   const base::FilePath rewards_base_path_;
   std::unique_ptr<PublisherInfoDatabase> publisher_info_backend_;
+  std::unique_ptr<RewardsDatabase> rewards_database_;
   std::unique_ptr<RewardsNotificationServiceImpl> notification_service_;
   base::ObserverList<RewardsServicePrivateObserver> private_observers_;
 #if BUILDFLAG(ENABLE_EXTENSIONS)
